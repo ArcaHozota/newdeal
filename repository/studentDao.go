adb84a6027a5b4c72147af8d6a1b35e046e90115
ArkamaHozota <arkamahozota@gmail.com>
2025-04-17 11:58:12 +0900

    1.02

--- repository/studentDao.go
+++ repository/studentDao.go
@@ -3,19 +3,19 @@ package repository
 import "newdeal/models"
 
 func findStudentsByEmail(email string) []models.Student {
-	students := []models.Student{}
+	var students []models.Student
 	models.DB.Where("visible_flg = true AND (login_account = ? OR email = ?)", email, email).Find(&students)
 	return students
 }
 
 func getStudentById(id int64) []models.Student {
-	student := []models.Student{}
+	var student []models.Student
 	models.DB.First(&student, models.Student{VisibleFlg: true, Id: id})
 	return student
 }
 
 func countStudentsByAccount(account string, id int64) int64 {
-	var kensu int64
-	models.DB.Where(&models.Student{VisibleFlg: true, LoginAccount: account}).Not(&models.Student{Id: id}).Count(&kensu)
-	return kensu
+	var kennsu int64
+	models.DB.Where(&models.Student{VisibleFlg: true, LoginAccount: account}).Not(&models.Student{Id: id}).Count(&kennsu)
+	return kennsu
 }
